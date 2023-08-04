a93c25fc3484a1efdf34f3450533b3d0dd0da095
Laura Gutierrez Funderburk <lgutierrwr@gmail.com>
2023-08-03 17:20:40 -0700

    postgres video

---
+++ docs/video-material/postgres/postgres.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv(".penv")
+
+import os
+
+# read environment variables
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
