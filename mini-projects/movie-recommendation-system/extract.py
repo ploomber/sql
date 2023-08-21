44aab80bf3dc62c822c88c01da62359d3ff6652d
jpjon <jonathanphan@berkeley.edu>
2023-08-21 10:17:18 -0700

    linting errors

--- mini-projects/movie-recommendation-system/extract.py
+++ mini-projects/movie-recommendation-system/extract.py
@@ -3,10 +3,11 @@ import requests
 
 # Parameter to get 500 English movies
 language_count = {
-    'en':500, 
+    "en": 500,
 }
 
-api_key = ''
+api_key = ""
+
 
 def init_duck_db(duckdb_file_path, res):
     """
@@ -21,8 +22,9 @@ def init_duck_db(duckdb_file_path, res):
     conn = duckdb.connect(duckdb_file_path, read_only=False)
 
     tables = conn.execute("SHOW TABLES;").fetchall()
-    if ('movies',) not in tables:
-        conn.execute("""
+    if ("movies",) not in tables:
+        conn.execute(
+            """
             CREATE TABLE movies (
                 genre_ids INTEGER[],
                 id INTEGER,
@@ -34,64 +36,85 @@ def init_duck_db(duckdb_file_path, res):
                 vote_average FLOAT,
                 vote_count INTEGER
             );
-        """)
+        """
+        )
 
     # Step 4: Insert the results from the API into the movies table
-    for movie in res['results']:
-        genre_ids_str = ",".join(map(str, movie['genre_ids'])) 
-        conn.execute(f"""
-            INSERT INTO movies VALUES (ARRAY[{genre_ids_str}], {movie['id']}, '{movie['original_language']}', '{movie['overview'].replace("'", "''")}', {movie['popularity']}, '{movie['release_date']}', '{movie['title'].replace("'", "''")}', {movie['vote_average']}, {movie['vote_count']});
-        """)
-
+    for movie in res["results"]:
+        genre_ids_str = ",".join(map(str, movie["genre_ids"]))
+        conn.execute(
+            f"""
+            INSERT INTO movies VALUES (ARRAY[{genre_ids_str}], {movie['id']},
+            '{movie['original_language']}',
+            '{movie['overview'].replace("'", "''")}',
+            {movie['popularity']},
+            '{movie['release_date']}',
+            '{movie['title'].replace("'", "''")}',
+            {movie['vote_average']},
+            {movie['vote_count']});
+        """
+        )
 
     conn.close()
-    
+
+
 def drop_existing_table(duckdb_file_path):
     conn = duckdb.connect(duckdb_file_path, read_only=False)
-    conn.execute("DROP TABLE movies;")
+
+    table_exists = conn.execute(
+        "SELECT 1 FROM information_schema.tables WHERE table_name = 'movies'"
+    ).fetchone()
+
+    if table_exists:
+        conn.execute("DROP TABLE movies;")
+        print("Table 'movies' dropped.")
+    else:
+        print("Table 'movies' does not exist.")
+
     conn.close()
-    
+
+
 def get_movies(lang, freq, duckdb_file_path):
-  url = 'https://api.themoviedb.org/3/movie/popular?api_key={api_key}&with_original_language={lang}'.format(api_key=api_key,lang=lang)
-  movies = 0
-  page = 1
-  progress = 0
-  
-  drop_existing_table(duckdb_file_path)
-  
-  while movies<freq:
-    try:
-        res = requests.get(url+"&page="+str(page))
-    except:
-        raise ('not connected to internet or movidb issue')
+    url = "https://api.themoviedb.org/3/movie/popular?api_key={api_key}&with_original_language={lang}".format(  # noqa E501
+        api_key=api_key, lang=lang
+    )
+    movies = 0
+    page = 1
+    progress = 0
 
-    if res.status_code != 200:
-        print ('error')
-        return []
+    drop_existing_table(duckdb_file_path)
 
-    res = res.json()
-    
-    if 'errors' in res.keys():
-      print('api error !!!')
-      return movies
+    while movies < freq:
+        try:
+            res = requests.get(url + "&page=" + str(page))
+        except requests.exceptions.RequestException as e:
+            print("An error occurred during the request:", e)
+            break
+        if res.status_code != 200:
+            print("error")
+            return []
 
-    movies +=  len(res['results'])
+        res = res.json()
 
-    init_duck_db(duckdb_file_path, res)
-    
-    if progress != round(movies/freq*100):
-      progress = round(movies/freq*100)
-      if progress%5==0:
-        print( progress, end="%, ")
-      
-    
-    
-    page = page + 1
-  return movies
+        if "errors" in res.keys():
+            print("api error !!!")
+            return movies
+
+        movies += len(res["results"])
+
+        init_duck_db(duckdb_file_path, res)
+
+        if progress != round(movies / freq * 100):
+            progress = round(movies / freq * 100)
+            if progress % 5 == 0:
+                print(progress, end="%, ")
+
+        page = page + 1
+    return movies
+
 
 for key in language_count:
-  # print(key,language_count[key])
-  print("Downloading", key, end=": ")
-  movies = get_movies(key,language_count[key], "movies.duckdb")
-  print('Total movies found:', movies)
-
+    # print(key,language_count[key])
+    print("Downloading", key, end=": ")
+    movies = get_movies(key, language_count[key], "movies.duckdb")
+    print("Total movies found:", movies)
