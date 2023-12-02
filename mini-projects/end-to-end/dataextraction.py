8fb0f8b7b0b4a70c4ed2aeecaa0bce3c936228ba
Laura Gutierrez Funderburk <lgutierrwr@gmail.com>
2023-12-01 17:48:43 -0800

    run format

--- mini-projects/end-to-end/dataextraction.py
+++ mini-projects/end-to-end/dataextraction.py
@@ -1,19 +1,19 @@
 import requests
 import pandas as pd
 from dotenv import load_dotenv
-import os 
+import os
 
-def extract_weather_by_lat_lon(api_key,lat, lon):
 
+def extract_weather_by_lat_lon(api_key, lat, lon):
     try:
         # Perform call
         url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 
-        querystring = {"q":f"{lat},{lon}","days":"5"}
+        querystring = {"q": f"{lat},{lon}", "days": "5"}
 
         headers = {
             "X-RapidAPI-Key": api_key,
-            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
         }
 
         response = requests.get(url, headers=headers, params=querystring)
@@ -22,15 +22,18 @@ def extract_weather_by_lat_lon(api_key,lat, lon):
     except requests.exceptions.HTTPError as e:
         print(e.response.text)
         return {}
-    
+
+
 def transform_json_to_dataframe(response):
     try:
+        unnested_df = pd.json_normalize(
+            response,
+            record_path=["forecast", "forecastday", "hour"],
+            meta=["location", "current"],
+        )
+        unnested_df.drop(columns=["location", "current"], inplace=True)
 
-        unnested_df = pd.json_normalize(response, record_path=['forecast','forecastday','hour'], meta=['location','current'])
-        unnested_df.drop(columns=['location','current'], inplace=True)
-
-        location_df = pd.json_normalize(response['location'])
-
+        location_df = pd.json_normalize(response["location"])
 
         for col_name in location_df.columns:
             unnested_df[col_name] = location_df[col_name][0]
@@ -43,20 +46,59 @@ def transform_json_to_dataframe(response):
     except Exception as e:
         print("Other Error:", e)
         return pd.DataFrame()
-    
 
-def extraction_df_lat_lon(api_key,lat, lon):
-    response = extract_weather_by_lat_lon(api_key,lat, lon)
+
+def extraction_df_lat_lon(api_key, lat, lon):
+    response = extract_weather_by_lat_lon(api_key, lat, lon)
     return transform_json_to_dataframe(response)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # Load api key
     load_dotenv()
     api_key = os.getenv("RapidAPI")
 
     # Extract data
-    latitudes = [40.7128, 34.0522, 43.6532, -23.5505, -34.6037, 4.7110, 51.5074, 48.8566, 52.5200, 35.6762, 39.9042, 19.0760, 30.0444, 6.5244, -26.2041, -33.8688, -37.8136, -27.4698]
-    longitudes = [-74.0060, -118.2437, -79.3832, -46.6333, -58.3816, -74.0721, -0.1278, 2.3522, 13.4050, 139.6503, 116.4074, 72.8777, 31.2357, 3.3792, 28.0473, 151.2093, 144.9631, 153.0251]
+    latitudes = [
+        40.7128,
+        34.0522,
+        43.6532,
+        -23.5505,
+        -34.6037,
+        4.7110,
+        51.5074,
+        48.8566,
+        52.5200,
+        35.6762,
+        39.9042,
+        19.0760,
+        30.0444,
+        6.5244,
+        -26.2041,
+        -33.8688,
+        -37.8136,
+        -27.4698,
+    ]
+    longitudes = [
+        -74.0060,
+        -118.2437,
+        -79.3832,
+        -46.6333,
+        -58.3816,
+        -74.0721,
+        -0.1278,
+        2.3522,
+        13.4050,
+        139.6503,
+        116.4074,
+        72.8777,
+        31.2357,
+        3.3792,
+        28.0473,
+        151.2093,
+        144.9631,
+        153.0251,
+    ]
     master_list = []
     for lat, lon in zip(latitudes, longitudes):
         master_list.append(extraction_df_lat_lon(api_key, lat, lon))
@@ -65,4 +107,4 @@ if __name__=="__main__":
     df = pd.concat(master_list)
 
     # Save to csv
-    df.to_csv('weather.csv', index=False)
+    df.to_csv("weather.csv", index=False)
